7d08fa262dd24444e52ac543f3b73b49af7e9a3d
조은채 <83952073+whdmsco010@users.noreply.github.com>
2023-04-03 20:13:25 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+s = int(input())
+if (s>=90 and s<=100):
+    print('A')
+elif (s>=80 and s<=89):
+    print('B')
+elif (s>=70 and s<=79):
+    print('C')
+elif (s>=60 and s<=69):
+    print('D')
+else:
+    print('F')

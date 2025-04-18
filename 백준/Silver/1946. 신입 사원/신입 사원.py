07d4ae8949939e1d064aa1ce8561485f476dfa80
조은채 <83952073+whdmsco010@users.noreply.github.com>
2025-04-18 20:18:58 +0900

    [Silver I] Title: 신입 사원, Time: 4052 ms, Memory: 211960 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -5,15 +5,11 @@ for _ in range(N):
     for _ in range(n):
         a, b = input().split()
         l.append((int(a), int(b)))
-    
     l.sort()
-
-    cnt = 1
-    f = l[0][1]
-
+    cnt = 1 # 가장 처음 사람은 무조건건 합격
+    f = l[0][1] # 첫 사람의 면접 코드
     for i in range(1, n):
         if l[i][1] < f:
             cnt += 1
             f = l[i][1]
-
     print(cnt)
